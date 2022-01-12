b770709389b46c0a74edc48803d2f5a10136bf9d
metastable-void <git@yuka.h09.eu>
2022-01-12 17:03:34 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 mod utils;
+extern crate sidh;
+extern crate rand;
 
 use wasm_bindgen::prelude::*;
+
+use rand::thread_rng;
+use sidh::sidh::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -16,4 +21,13 @@ extern {
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, sidh-kex!");
+}
+
+#[wasm_bindgen]
+pub fn generate_alice_keypair() {
+    let mut rng = thread_rng();
+    let (alice_public, alice_secret) = generate_alice_keypair(&mut rng);
+    let alice_public_bytes = alice_public.to_bytes();
+    let alice_secret_bytes = alice_secret.to_bytes();
+    return (alice_public_bytes, alice_secret_bytes);
 }
